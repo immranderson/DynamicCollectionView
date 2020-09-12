4df3369143bd86f6f3dfb37eaf5770b13469dd7b
tomdroid <immranderson@gmail.com>
2020-09-12 17:59:06 -0500

    Initial Commit

---
+++ DynamicCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicCollectionView
+//
+//  Created by Tom Anderson on 9/12/20.
+//  Copyright © 2020 Tom Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
